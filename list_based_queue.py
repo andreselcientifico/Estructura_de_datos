f042bdcd21bdcb60fcf5dca94468023d36a3a992
andres perez <andresperezrodriguez28@gmail.com>
2023-04-10 20:18:26 -0500

    List based queue

---
+++ list_based_queue.py
@@ -0,0 +1,28 @@
+
+class listQueue:
+    def __init__(self) -> None:
+        self.items = []
+        self.size = 0
+
+    def enqueue(self, data):
+        self.items.insert(0,data)
+        self.size += 1
+
+    def dequeue(self):
+        data = self.items.pop()
+        self.size -= 1
+        return data
+    
+    def traverse(self):
+        total_items = self.size
+        print(range(total_items))
+        for item in range(total_items):
+            print(self.items[item])
+
+if __name__ == '__main__':
+    food = listQueue()
+    food.enqueue('eggs')
+    food.enqueue('ham')
+    food.enqueue('spam')
+    print(food.dequeue())
+    food.traverse()
